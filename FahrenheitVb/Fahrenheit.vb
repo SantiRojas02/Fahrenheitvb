5a06fd4e9d4205641cd53f70a0249ef7c49768ca
SantiRojas <sr2897040@gmail.com>
2022-05-31 13:48:36 -0300

    FahrenheitVb

---
+++ FahrenheitVb/Fahrenheit.vb
@@ -0,0 +1,17 @@
+﻿Module Fahrenheit
+
+    Sub Main(args As String())
+        'Declaracion de variables
+        Dim temperaturaCelcius As Single
+        Dim temperaturaFahrenheit As Single
+        'Ingresamos los datos
+        Console.Write("Ingresar temperatura en grados celcius: ")
+        temperaturaCelcius = Console.ReadLine()
+        ' Operaciones
+        temperaturaFahrenheit = temperaturaCelcius * 1.8 + 32
+        ' Resultado
+        Console.WriteLine("Equivale a {temperaturaFahrenheit} grados Fahrenheit")
+
+    End Sub
+
+End Module
